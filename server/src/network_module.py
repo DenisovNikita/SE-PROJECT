f8a0a4ebdb8d3b2aaa17d11137e42bb2c84a9fb1
Nikita Denisov <nikita160412@yandex.ru>
2022-10-19 14:13:13 +0300

    Add first endpoint list_files

--- server/src/network_module.py
+++ server/src/network_module.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
+from os import listdir
+from os.path import isfile, join
 
 
 app = FastAPI()
+folder_with_files = '../../user_files/'
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/list_files/")
+async def list_files():
+    return [f for f in listdir(folder_with_files) if isfile(join(folder_with_files, f))]
