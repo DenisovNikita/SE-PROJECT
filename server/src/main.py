d2544c0a6308960b5f6b99a09cf66b306440ae7a
Nikita Denisov <nikita160412@yandex.ru>
2022-10-19 14:08:00 +0300

    Add uvicorn fastapi server

--- server/src/main.py
+++ server/src/main.py
@@ -1,3 +1,5 @@
+from network_module import run_server
+
 def sum(a, b):
     return a + b
 
@@ -6,6 +8,8 @@ def main():
     a = 2
     b = 2
     print(f'sum({a}, {b}) = {sum(a, b)}')
+
+    run_server()
 
 
 if __name__ == "__main__":
