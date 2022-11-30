f8fea5049547605d88e9026e50506f6558e4dfc8
Nikita Denisov <nikita160412@yandex.ru>
2022-11-30 14:15:35 +0300

    Add documentation for entites

--- server/src/entities.py
+++ server/src/entities.py
@@ -2,5 +2,13 @@ from pydantic import BaseModel
 
 
 class File(BaseModel):
+    """
+
+    File have a filename and data inside it.
+    Filename could be any appropriate string.
+    Data must be ascii symbols.
+
+    """
+
     filename: str
     data: str
