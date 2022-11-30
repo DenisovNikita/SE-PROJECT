5f1dda8ebf745b8a683974e8c95f0e84648059ea
Nikita Denisov <nikita160412@yandex.ru>
2022-11-30 15:29:01 +0300

    Refactor & add documentation for tests on endpoints

--- server/tests/test_main.py
+++ server/tests/test_main.py
@@ -1,30 +1,58 @@
 import pytest
+from requests import Response
 
 from server.src.main import sum
 import requests
 
 
-def test_main():
+def test_main() -> None:
+    """Test for checking pytest workability.
+
+    Returns:
+        None.
+
+    """
     assert sum(2, 2) == 4, "kek"
 
 
-url = 'http://localhost:8000/'
-data = 'data'
+url: str = 'http://localhost:8000/'
+data: str = 'data'
 
 
-def check_status_code(r):
+def check_status_code(r: Response) -> None:
+    """Test for checking status code of network response.
+
+    Args:
+        r (Response): Network response.
+
+    Returns:
+        None.
+
+    """
     assert r.status_code == 200, f'expected 200, but status_code = {r.status_code}'
 
 
-def test_list_files():
+def test_list_files() -> None:
+    """Test for endpoint `list_files`.
+
+    Returns:
+        None.
+
+    """
     path = 'list_files'
-    r = requests.get(url + path)
+    r: Response = requests.get(url + path)
     check_status_code(r)
     assert r.json() == ['test1', 'test2'], f'expected empty list, but have {r.json()}'
 
 
 @pytest.fixture
-def filename():
+def filename() -> str:
+    """Experiment with fixtures.
+
+    Returns:
+        str: Generated filename.
+
+    """
     return 'kek'
 
 
